d5fcd951d59faebd54cc213b8c03d2892e5f905b
ReyhaneSaljooghi <reysalphimo@gmail.com>
2024-05-20 15:36:16 +0330

    sonar code review changes

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import Button from './Common/Button'
 import { useSelector } from 'react-redux';
 import { MdOutlineEdit } from "react-icons/md";
-import { CiSquarePlus } from "react-icons/ci";
 import { LuFolderArchive } from "react-icons/lu";
 import { RxExit } from "react-icons/rx";
 import { IoSearchOutline } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa6";
-import { GiHamburgerMenu } from "react-icons/gi";
 import AdsItem from './Common/AdsItem'
 export default function Profile() {
 
@@ -57,15 +55,16 @@ export default function Profile() {
     
       <div className=' hidden med:block med:basis-[72%] mt-5 pr-16'>
         
-          <form class="w-[37%] mb-8">   
-              <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-              <div class="relative min-h-0 h-full">
-                  <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                      <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <form className="w-[37%] mb-8">   
+              <div className="relative min-h-0 h-full">
+                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                      <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                       </svg>
                   </div>
-                  <input type="search" id="default-search" class="block w-full h-[42px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="آگهیت رو جستجو کن!" required />
+              
+                  <input type="search" id="default-search" className="block w-full h-[42px] p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="آگهیت رو جستجو کن!" required />
+              
                   <Button type="submit" text="جستجو" className='h-[80%] !w-1/4 shadow-2xl text-sm text-white text-center absolute end-2.5 bottom-1 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'  />
               </div>
           </form>
