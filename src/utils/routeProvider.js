f9178782b927fcc6540025211fb0bd67f45d7cd1
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2024-05-23 21:45:49 +0330

    rputer changed

--- src/utils/routeProvider.js
+++ src/utils/routeProvider.js
@@ -31,7 +31,7 @@ export const router = createBrowserRouter([
         element: React.createElement(SubmitAds)
       },
       {
-        path: "/call-us",
+        path: "/contact-us",
       element:React.createElement(CallUs),
       }
     ],
