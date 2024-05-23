23932352b5f3687d04bdcfb340eddb945e574b45
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2024-05-23 11:49:01 +0330

    submitHandler posts data

--- src/Components/SubmitAds.jsx
+++ src/Components/SubmitAds.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import InputForm from "./Common/InputForm";
+import IconButton from "./Common/IconButton";
+import { ImStatsDots } from "react-icons/im";
 
 export default function SubmitAds() {
     const [formData, setFormData] = useState({
@@ -22,12 +24,51 @@ export default function SubmitAds() {
         video: null
     });
 
-    function handleFormSubmit(event) {
+    async function handleFormSubmit(event) {
         event.preventDefault();
-        // submit formData to backend
-        console.log("Form submitted with data:", formData);
-        // You can reset the form data if needed
-        // setFormData({}); 
+
+        const data = new FormData();
+        for (const key in formData) {
+            data.append(key, formData[key]);
+        }
+
+        try {
+            const response = await fetch('https://your-backend-endpoint.com/api/submit', {
+                method: 'POST',
+                body: data
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json();
+            console.log('Form successfully submitted:', result);
+
+            // Optionally reset the form
+            // setFormData({
+            //     city: "",
+            //     parish: "",
+            //     address: "",
+            //     brand: "",
+            //     carColor: "",
+            //     year: "",
+            //     output: "",
+            //     price: "",
+            //     frame: "",
+            //     motor: "",
+            //     chassis: "",
+            //     chassisBack: "",
+            //     chassisFront: "",
+            //     insurance: "",
+            //     number: "",
+            //     picture: null,
+            //     video: null
+            // });
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
     }
 
     function handleChange(event) {
@@ -37,6 +78,7 @@ export default function SubmitAds() {
             [name]: value
         });
     }
+
     function handlePictureChange(event) {
         const pictureFile = event.target.files[0];
         setFormData({
@@ -54,114 +96,162 @@ export default function SubmitAds() {
     }
 
     return (
-        <div dir="rtl" className="flex flex-row items-center font-dast">
-            <div className="container">
-                <form className="form" onSubmit={handleFormSubmit}>
-                    <div>محل سکونت</div>
+        <div dir="rtl" className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+                <form className="space-y-4" onSubmit={handleFormSubmit}>
+                    <h2 className="text-2xl font-bold mb-4">ثبت آگهی</h2>
+                    <div className="text-lg font-semibold">محل سکونت</div>
                     <InputForm
                         placeHolder={"شهر"}
-                        type={"city"}
+                        type={"text"}
                         handleChange={handleChange}
                         fieldkey={"city"}
                         name={"city"}
                         value={formData.city}
+                        className="w-full"
                     />
-                      <InputForm
-            placeHolder={"محله"}
-              type={"parish"}
-              handleChange={handleChange}
-              fieldkey={"parish"}
-            />
-            <InputForm
-              placeHolder={"آدرس"}
-              type={"address"}
-              handleChange={handleChange}
-              fieldkey={"address"}
-            />
-            <div>مشخصات ظاهری</div>
-            <InputForm
-              placeHolder={"برند و تیپ"}
-              type={"brand"}
-              handleChange={handleChange}
-              fieldkey={"brand"}
-            />
-             <InputForm
-              placeHolder={"رنگ"}
-              type={"carColor"}
-              handleChange={handleChange}
-              fieldkey={"carColor"}
-            />
-             <InputForm
-              placeHolder={"سال تولید"}
-              type={"year"}
-              handleChange={handleChange}
-              fieldkey={"year"}
-            />
-             <InputForm
-              placeHolder={"کارکرد"}
-              type={"output"}
-              handleChange={handleChange}
-              fieldkey={"output"}
-            />
-             <InputForm
-              placeHolder={"قیمت"}
-              type={"price"}
-              handleChange={handleChange}
-              fieldkey={"price"}
-            />
-            <InputForm
-              placeHolder={"مشخصات بدنه"}
-              type={"frame"}
-              handleChange={handleChange}
-              fieldkey={"frame"}
-            />
-            <div>مشخصات تکمیلی</div>
-            <InputForm
-              placeHolder={"وضعیت موتور"}
-              type={"motor"}
-              handleChange={handleChange}
-              fieldkey={"motor"}
-            />
-            <InputForm
-              placeHolder={"وضعیت شاسی عقب"}
-              type={"chassis"}
-              handleChange={handleChange}
-              fieldkey={"chassis"}
-            />
-            <InputForm
-              placeHolder={"وضعیت شاسی عقب"}
-              type={"chassisBack"}
-              handleChange={handleChange}
-              fieldkey={"chassisBack"}
-            />
-            <InputForm
-              placeHolder={"وضعیت شاسی جلو"}
-              type={"chassisFront"}
-              handleChange={handleChange}
-              fieldkey={"chassisFront"}
-            />
-            <InputForm
-              placeHolder={"مهلت بیمه شخص ثالث"}
-              type={"insurance"}
-              handleChange={handleChange}
-              fieldkey={"insurance"}
-            />
-            <div>مشخصات تماس</div>
-            <InputForm
-              placeHolder={"شماره تماس"}
-              type={"number"}
-              handleChange={handleChange}
-              fieldkey={"number"}
-            />
-             <div>آپلود عکس</div>
-             <input type="file" accept="image/*" onChange={handlePictureChange} />       
-             
-             <div>آپلود ویدیو</div>
-             <input type="file" accept="video/*" onChange={handleVideoChange} />
-                    <button type="submit">Submit</button>
+                    <InputForm
+                        placeHolder={"محله"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"parish"}
+                        name={"parish"}
+                        value={formData.parish}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"آدرس"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"address"}
+                        name={"address"}
+                        value={formData.address}
+                        className="w-full"
+                    />
+                    <div className="text-lg font-semibold">مشخصات ظاهری</div>
+                    <InputForm
+                        placeHolder={"برند و تیپ"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"brand"}
+                        name={"brand"}
+                        value={formData.brand}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"رنگ"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"carColor"}
+                        name={"carColor"}
+                        value={formData.carColor}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"سال تولید"}
+                        type={"number"}
+                        handleChange={handleChange}
+                        fieldkey={"year"}
+                        name={"year"}
+                        value={formData.year}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"کارکرد"}
+                        type={"number"}
+                        handleChange={handleChange}
+                        fieldkey={"output"}
+                        name={"output"}
+                        value={formData.output}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"قیمت"}
+                        type={"number"}
+                        handleChange={handleChange}
+                        fieldkey={"price"}
+                        name={"price"}
+                        value={formData.price}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"مشخصات بدنه"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"frame"}
+                        name={"frame"}
+                        value={formData.frame}
+                        className="w-full"
+                    />
+                    <div className="text-lg font-semibold">مشخصات تکمیلی</div>
+                    <InputForm
+                        placeHolder={"وضعیت موتور"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"motor"}
+                        name={"motor"}
+                        value={formData.motor}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"وضعیت شاسی"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"chassis"}
+                        name={"chassis"}
+                        value={formData.chassis}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"وضعیت شاسی عقب"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"chassisBack"}
+                        name={"chassisBack"}
+                        value={formData.chassisBack}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"وضعیت شاسی جلو"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"chassisFront"}
+                        name={"chassisFront"}
+                        value={formData.chassisFront}
+                        className="w-full"
+                    />
+                    <InputForm
+                        placeHolder={"مهلت بیمه شخص ثالث"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"insurance"}
+                        name={"insurance"}
+                        value={formData.insurance}
+                        className="w-full"
+                    />
+                    <div className="text-lg font-semibold">مشخصات تماس</div>
+                    <InputForm
+                        placeHolder={"شماره تماس"}
+                        type={"text"}
+                        handleChange={handleChange}
+                        fieldkey={"number"}
+                        name={"number"}
+                        value={formData.number}
+                        className="w-full"
+                    />
+                    <div className="text-lg font-semibold">آپلود عکس</div>
+                    <input type="file" accept="image/*" onChange={handlePictureChange} className="w-full mb-4" />
+                    <div className="text-lg font-semibold">آپلود ویدیو</div>
+                    <input type="file" accept="video/*" onChange={handleVideoChange} className="w-full mb-4" />
+                    <button
+                        type="submit"
+                        className="flex items-center justify-center bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-200"
+                    >
+                        <ImStatsDots className="mr-2" />
+                        ثبت آگهی
+                    </button>
                 </form>
             </div>
         </div>
     );
 }
-
