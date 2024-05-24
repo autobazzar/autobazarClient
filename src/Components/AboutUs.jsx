ba9d05d5fbe47971618fec59c7a63a5dcf71b0cf
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2024-05-24 23:23:36 +0330

    some changes in UI

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,22 +5,22 @@ export default function AboutUs() {
     <div dir="rtl" className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="p-6 rounded-lg shadow-lg w-full max-w-4xl bg-white">
         <div className="flex flex-col md:flex-row">
-          <div className="md:w-1/4 p-4">
+          <div className="md:w-1/4 p-4 order-2 md:order-1">
             <h2 className="text-2xl font-bold mb-4">درباری اتوبازار</h2>
             <p className="mb-4">
               اتوبازار از اسفند ماه 1400 با هدف ایجاد سهولت در خرید و فروش ماشین آغار به کار کرد.
             </p>
           </div>
-          <div className="md:w-1/4 p-4 flex justify-center border-8 border-green-750">
-            <img src={contactImage} alt="About Us" className="rounded-lg shadow-lg max-h-96" />
-          </div>
         </div>
+        <div className="md:w-1/4 p-4 flex justify-start order-1 md:order-2">
+            <img src={contactImage} alt="About Us" className="rounded-lg shadow-lg w-90 h-44 object-cover" />
+          </div>
         <div className="mt-4">
           <p>
             اگر قصد خرید یا فروش ماشین خود را دارید، یافتن یک پلتفورم مناسب برای ایجاد آگهی به منظور فروش ماشین و جستجو برای خرید ماشین مورد نظر باید جزو اقدامات اولیه شما باشد.
-            </p>
-            <br/>
-            <p>
+          </p>
+          <br />
+          <p>
             اتوبازار یک پلتفورم معتبر و راحت است که می‌توانید اطلاعات مورد نظر درباره ماشین‌های فروشی را در آن بررسی کنید و همچنین اگر ماشینی برای فروش دارید، آگهی ایجاد کنید و به راحتی مشتری بیابید!
           </p>
         </div>
--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,44 +1,56 @@
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaMailBulk } from 'react-icons/fa';
 
 export default function ContactUS() {
   return (
-    <div dir="rtl" className="flex justify-center bg-gray-100 p-4 grid-cols-3 gap-10 pt-20" >
-        {/* Contact Us Section */}
-        <div className="p-6 rounded-lg shadow-lg  bg-white col-span-6">
-          <h2 className="text-2xl font-bold mb-4 text-black">تماس با ما</h2>
-          <div className="mb-2 flex items-center">
+    <div dir="rtl" className="flex justify-center bg-gray-100 p-4 pt-20">
+      {/* Contact Us Section */}
+      
+      <div className="p-6 col-span-6 grid-rows-3">
+      <h2 className="text-2xl font-bold mb-4 text-black">تماس با ما</h2>
+        <div className="mb-2">
+          <div className="flex items-center">
             <FaPhoneAlt className="text-black ml-2" />
             <span className="font-bold text-black">تلفن پشتیبانی:</span>
+          </div>
+          <div className="mt-2">
             <span className="text-black ml-2">29901</span>
           </div>
-          <div className="mb-2 flex items-center">
-            <FaEnvelope className="text-black ml-2" />
-            <span className="font-bold text-black">ایمیل:</span>
+        </div>
+        <div className="mb-2 flex items-center">
+          <FaEnvelope className="text-black ml-2" />
+          <span className="font-bold text-black">ایمیل:</span>
+        </div>
+        <div className="mt-2">
             <span className="text-black ml-2">pr-office@sbu.ac.ir</span>
-          </div>
-          <div className="mb-2 flex items-center">
-            <FaMapMarkerAlt className="text-black ml-2" />
-            <span className="font-bold text-black">کد پستی:</span>
-            <span className="text-black ml-2">1983969411</span>
-          </div>
-          </div>
-           {/* Address Section */}
-        <div className="p-6 rounded-lg shadow-lg  bg-white col-span-6">
-          <h2 className="text-2xl font-bold mb-4 text-black">نشانی</h2>
-          <div className="text-black mb-4">
-            تهران، اوین، میدان شهید شهریاری
-          </div>
-          <div className="bg-white h-64 rounded-lg shadow-inner">
-            {/* Map centered on Evin, Tehran */}
-            <iframe
-              title="Map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.4561442697943!2d51.37460811519291!3d35.78847358017013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e06b8cda65d37%3A0xa58b8c2b75bdb905!2sEvin%2C%20Tehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2sus!4v1682577406497!5m2!1sen!2sus"
-              className="w-full h-full rounded-lg"
-              allowFullScreen=""
-              loading="lazy"
-            ></iframe>
-          </div>
-        </div>       
+        </div>
+        <div className="mb-2 flex items-center">
+          <FaMailBulk className="text-black ml-2" />
+          <span className="font-bold text-black">کد پستی:</span>
+        </div>
+        <div className="mt-2">
+          <span className="text-black ml-2">1983969411</span>
+        </div>
       </div>
+      {/* Address Section */}
+      <div className="p-6 col-span-6 pt-10">
+        <div className="mb-2 flex items-center">
+          <FaMapMarkerAlt className="text-black ml-2" />
+          <span className="font-bold text-black">نشانی:</span>
+        </div>
+        <div className="text-black mb-4">
+          تهران، اوین، میدان شهید شهریاری
+        </div>
+        <div className="bg-white h-64 rounded-lg shadow-inner">
+          {/* Map centered on Evin, Tehran */}
+          <iframe
+            title="Map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.4561442697943!2d51.37460811519291!3d35.78847358017013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e06b8cda65d37%3A0xa58b8c2b75bdb905!2sEvin%2C%20Tehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2sus!4v1682577406497!5m2!1sen!2sus"
+            className="w-full h-full rounded-lg"
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
+    </div>
   );
 }
