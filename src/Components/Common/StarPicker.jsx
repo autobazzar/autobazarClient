1561804e6d37874436c57ece2a199775ea7d3220
iliya Akhoondi <akhoondiiliya@gmail.com>
2024-05-25 23:51:45 +0430

    rating ads added

--- src/Components/Common/StarPicker.jsx
+++ src/Components/Common/StarPicker.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
-
-export default function StarPicker() {
+import { string, number } from "prop-types";
+import { submitScore } from "../../api/api";
+export default function StarPicker({user_id, ad_id}) {
     const [choosenStar,setChoosenStar]= useState(-1);
+    const handleSubmit = async () => {
+      const payload = {
+          score: choosenStar,
+          userId: user_id,
+          adId: ad_id
+      };
+      
+      console.log(payload);
+      await submitScore(payload);
+      window.location.reload();
+  };
   return (
     <div className="flex flex-col gap-2 justify-center">
 
@@ -21,7 +33,13 @@ export default function StarPicker() {
           </button>
         ))}
         </div>
-        <button className="border-[1px] border-gray-400 w-fit self-center py-1 px-10 text-gray-700 rounded-lg shadow-xl">ثبت امتیاز</button>
+        <button onClick={handleSubmit} onKeyDown={handleSubmit} className="border-[1px] border-gray-400 bg-white hover:text-white hover:bg-[#2b4e47] w-fit self-center py-1 px-10 text-gray-700 rounded-lg shadow-xl">ثبت امتیاز</button>
     </div>
   );
 }
+
+StarPicker.propTypes = {
+  ad_id:string,
+  user_id: number,
+  
+};
