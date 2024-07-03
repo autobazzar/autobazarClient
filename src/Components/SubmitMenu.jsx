efa1d6c3a060d71d8160d3da642c1e957f39a571
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2024-07-04 01:19:05 +0330

    submit-ads updated

--- src/Components/SubmitMenu.jsx
+++ src/Components/SubmitMenu.jsx
@@ -1,16 +1,17 @@
 import MenuCard from "./MenuCard";
-import { IoLocationOutline,IoCall  } from "react-icons/io5";
+import { IoLocationOutline, IoCall } from "react-icons/io5";
 import { GrPowerForceShutdown } from "react-icons/gr";
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
+
 export default function SubmitMenu() {
   return (
-    <div className="hidden med:flex bg-[#F1F5F6] h-fit w-fit p-10 rounded-xl">
+    <div className="hidden med:flex flex-col items-center bg-[#F1F5F6] h-fit w-fit p-10 rounded-xl">
       <h3 className="text-2xl font-bold text-nowrap font-dast mb-6">
         مراحل ثبت آگهی
       </h3>
       <div className="flex flex-col gap-5">
         <MenuCard
-        className={'!bg-[#2B4E47] text-white'}
+          className={'!bg-[#2B4E47] text-white'}
           title={"محل سکونت"}
           icon={<IoLocationOutline className="text-2xl" />}
         />
