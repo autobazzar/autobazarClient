4adac07a0117d92723c34e4a537ff2c6ef1b300c
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2024-07-04 00:49:14 +0330

    contactUs and aboutUs updated

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -12,10 +12,10 @@ export default function AboutUs() {
             </p>
           </div>
         </div>
-        <div className="md:w-1/4 p-4 flex justify-start order-1 md:order-2">
+        <div className="hidden med:flex md:w-1/4 p-4 flex justify-start order-1 md:order-2">
             <img src={contactImage} alt="About Us" className="rounded-lg shadow-lg w-90 h-44 object-cover" />
           </div>
-        <div className="mt-4">
+        <div className="mt-4 justify-center items-center pl-10">
           <p>
             اگر قصد خرید یا فروش ماشین خود را دارید، یافتن یک پلتفورم مناسب برای ایجاد آگهی به منظور فروش ماشین و جستجو برای خرید ماشین مورد نظر باید جزو اقدامات اولیه شما باشد.
           </p>
